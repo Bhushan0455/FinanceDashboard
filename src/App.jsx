bab68bcd1bfd5847f55811c4c9154a8d12d320d6
Bhushan0455 <bhushanparab7870@gamil.com>
2026-04-04 12:34:32 +0530

    Step 1: Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Finance Dashboard 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
